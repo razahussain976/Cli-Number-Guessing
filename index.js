9f3029c0bef126765d437ad7223b872a6d83e732
razahussain976 <raza.hussain0010@gmail.com>
2024-03-25 04:09:42 +0500

    decrease random range & edit console

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const randomnumber = Math.floor(Math.random() * 10 + 1);
+const randomnumber = Math.floor(Math.random() * 5 + 1);
+console.log("Welcome to number guessing game!");
 const answer = await inquirer.prompt([
     {
         name: "userGuessNumber",
         type: "number",
-        message: "Please guess a number",
+        message: "Please guess a number between 1-6",
     }
 ]);
 if (randomnumber === answer.userGuessNumber) {
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 
 import inquirer from "inquirer";
 
-const randomnumber =Math.floor( Math.random() * 10 + 1);
+const randomnumber =Math.floor( Math.random() * 5 + 1);
 
 console.log("Welcome to number guessing game!")
 
@@ -10,7 +10,7 @@ const answer = await inquirer.prompt([
     {
         name: "userGuessNumber",
         type: "number",
-        message:"Please guess a number",
+        message:"Please guess a number between 1-6",
     }
 ]);
 
